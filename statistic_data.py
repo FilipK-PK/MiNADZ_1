c8f5b2574b7e1b6fc993afd072a3d44a8015fb9b
Filip Kostkiewicz <ftsk.hj@wp.pl>
2022-11-28 12:18:04 +0100

    statystyki - autokorelacja

--- statistic_data.py
+++ statistic_data.py
@@ -1,7 +1,7 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-
+import math as m
 
 class Statistic:
 
@@ -24,4 +24,50 @@ class Statistic:
         plt.title('Histogram')
         plt.hist(data)
 
+        plt.subplot(224)
+        plt.title('Autokorelacja')
+        plt.plot(self.corel(data))
+
         plt.show()
+
+    def corel(self, data_old, abs=False, len_w=False):
+        correlation_data = []
+
+        data = data_old.copy()
+        data = data.values.reshape(-1)
+        data = self.__norm(self.__steps(data))
+
+        if not len_w:
+            if abs:
+                [correlation_data.append(np.corrcoef(np.absolute(data[0:-1 - i]), np.absolute(data[i:-1]))[0, 1]) for i
+                 in
+                 range(data.shape[0] // 2)]
+
+            else:
+                [correlation_data.append(np.corrcoef(data[0:-1 - i], data[i:-1])[0, 1]) for i in
+                 range(data.shape[0] // 2)]
+        else:
+            if abs:
+                [correlation_data.append(np.corrcoef(np.absolute(data[0:-1 - i]), np.absolute(data[i:-1]))[0, 1]) for i
+                 in
+                 range(len_w)]
+
+            else:
+                [correlation_data.append(np.corrcoef(data[0:-1 - i], data[i:-1])[0, 1]) for i in range(len_w)]
+
+        return correlation_data
+    def __steps(self, data, rand=False):
+        if rand:
+            d = []
+            for i in range(1, data.size):
+                try:
+                    d.append(m.log(data[i + 1] - data[i]))
+                except Exception:
+                    d.append(0.0)
+
+            return np.array(d)
+
+        return np.log(data[1:] / data[:-1])
+
+    def __norm(self, data):
+        return (data - data.mean()) / data.std()
